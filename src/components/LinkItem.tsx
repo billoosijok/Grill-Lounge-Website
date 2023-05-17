93bcd2add3af4ec404bf7f7fff7f1cec5ec0cf4f
Belal Sejouk <billoosijok@gmail.com>
2023-05-17 03:40:35 +0200

    Release

--- src/components/LinkItem.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const LinkItem = ({label, url, analyticsId}) => {
-  return <a href={url} target="_blank" onClick={() => global.gtag('event', analyticsId)}>{label}</a>
-}
