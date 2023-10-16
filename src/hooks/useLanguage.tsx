13ee82d2aa90e65c5c1dcb22de96a76796802a81
Belal Sejouk <billoosijok@gmail.com>
2023-10-16 14:41:42 +0200

    chore: useLanguage

---
+++ src/hooks/useLanguage.tsx
@@ -0,0 +1,30 @@
+import {createContext, useContext, useMemo, useState} from "react";
+
+type SupportedLanguages = 'fr'|'en';
+
+interface LanguageContextProps {
+  lang: SupportedLanguages;
+  setLang: (lang: SupportedLanguages) => void
+}
+
+const LanguageContext = createContext<LanguageContextProps|undefined>(undefined);
+
+export const useLanguage = () => {
+  const langContext = useContext(LanguageContext);
+
+  if (!langContext) throw new Error();
+
+  const {lang, setLang} =langContext
+
+  return useMemo(() => ({
+    lang, setLang
+  }),[lang, setLang])
+}
+
+export const LanguageProvider = ({children}) => {
+  const [lang, setLang] = useState<SupportedLanguages>('fr');
+
+  return <LanguageContext.Provider value={{ lang, setLang }}>
+          {children}
+        </LanguageContext.Provider>
+}
