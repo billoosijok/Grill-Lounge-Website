c793abf765ccd4da41174358f7d95b4832fcf7d8
Belal Sejouk <billoosijok@gmail.com>
2023-12-19 19:30:53 +0100

    Publish: $PROMPT_ENV

---
+++ prompt.js
@@ -0,0 +1,18 @@
+const { spawn, execSync } = require('child_process');
+const exec = commands => {
+    execSync(commands, { stdio: 'inherit', shell: true });
+};
+const spawnProcess = commands => {
+    spawn(commands, { stdio: 'inherit', shell: true });
+};
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter publish message', (input) => {
+    exec(`PROMPT_ENV=${input}`);
+    rl.close();
+});
